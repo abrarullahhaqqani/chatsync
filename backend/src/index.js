521b15bb447df5f8e5f846314e9b080be191a681
abrarullahhaqqani <abrarullahhaqqani@gmail.com>
2025-06-19 10:13:44 +0530

    test: comment production catch-all route

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,12 +28,12 @@ console.log("✅ Checking messageRoutes type:", typeof messageRoutes);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server is running at port ` + PORT);
